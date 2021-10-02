68776b2f8001d1e35bab33c2eca5e2b055d1c6c5
Sid Nair <siddharthnair87@outlook.com>
2021-10-02 18:44:02 +0100

    created queries for hadnling review requests

--- backend/app.js
+++ backend/app.js
@@ -15,6 +15,6 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
 
 // app.use("/", indexRouter);
-app.use("/movies", moviesRouter);
+app.use("/shows", moviesRouter);
 
 module.exports = app;
--- backend/models/movies.js
+++ backend/models/movies.js
@@ -2,8 +2,53 @@ const { query } = require("../db/index");
 
 async function getAllPopularMovies() {
   const data = await query(`SELECT * FROM popularMovies`);
-  console.log(data);
+  console.log(data.rows);
+  return data.rows;
+}
+async function getAllTopRatedMovies() {
+  const data = await query(`SELECT * FROM topRatedMovies`);
+  console.log(data.rows);
+  return data.rows;
+}
+async function getAllPopularTVShows() {
+  const data = await query(`SELECT * FROM popularTV`);
+  console.log(data.rows);
+  return data.rows;
+}
+async function getAllTopRatedTVShows() {
+  const data = await query(`SELECT * FROM topRatedTV`);
+  console.log(data.rows);
   return data.rows;
 }
 
-module.exports = { getAllPopularMovies };
+async function addReview(review, title, show) {
+  const data = await query(
+    `UPDATE popularMovies SET reviews = $1 WHERE title = '${title} RETURNING *;`,
+    [review]
+  );
+  return data.rows;
+}
+async function deleteReview(review, title, show) {
+  const data = await query(
+    `DELETE FROM popularMovies SET reviews = $1 WHERE title = '${title} RETURNING *;`,
+    [review]
+  );
+  return data.rows;
+}
+async function editReview(review, title, show) {
+  const data = await query(
+    `UPDATE popularMovies SET reviews = $1 WHERE title = '${title} RETURNING *;`,
+    [review]
+  );
+  return data.rows;
+}
+
+module.exports = {
+  getAllPopularMovies,
+  getAllPopularTVShows,
+  getAllTopRatedMovies,
+  getAllTopRatedTVShows,
+  addReview,
+  editReview,
+  deleteReview,
+};
