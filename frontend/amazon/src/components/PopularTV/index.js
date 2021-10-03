0356695434c00cf80ae7a9f61c76c28842c1c8ba
Sid Nair <siddharthnair87@outlook.com>
2021-10-03 18:46:28 +0100

    displayed overview and title on hover

--- frontend/amazon/src/components/PopularTV/index.js
+++ frontend/amazon/src/components/PopularTV/index.js
@@ -1,19 +1,29 @@
 import DisplayData from "../DisplayData";
 import RenderData from "../RenderData";
 import useFetch from "../useFetch";
+import { useState } from "react";
 
 const PopularTV = ({ title }) => {
+  const [name, setName] = useState("");
+  const [overview, setOverview] = useState("");
   const { data, isPending, error } = useFetch(
     "http://localhost:3000/shows/populartv"
   );
+  const handleHover = (name, overview) => {
+    setName(name);
+    setOverview(overview);
+  };
   return (
     <>
+      <h1>{name}</h1>
+      <h2>{overview}</h2>
       <h3>{title}</h3>
       <RenderData
         data={data}
         isPending={isPending}
         error={error}
         DisplayData={DisplayData}
+        handleHover={handleHover}
       />
     </>
   );
--- frontend/amazon/src/components/TopRatedTV/index.js
+++ frontend/amazon/src/components/TopRatedTV/index.js
@@ -1,20 +1,29 @@
-// import { useState, useEffect } from "react";
+import { useState } from "react";
 import DisplayData from "../DisplayData";
 import RenderData from "../RenderData";
 import useFetch from "../useFetch";
 
 const TopRatedTV = ({ title }) => {
+  const [name, setName] = useState("");
+  const [overview, setOverview] = useState("");
   const { data, isPending, error } = useFetch(
     "http://localhost:3000/shows/topratedtv"
   );
+  const handleHover = (name, overview) => {
+    setName(name);
+    setOverview(overview);
+  };
   return (
     <>
+      <h1>{name}</h1>
+      <h2>{overview}</h2>
       <h3>{title}</h3>
       <RenderData
         data={data}
         isPending={isPending}
         error={error}
         DisplayData={DisplayData}
+        handleHover={handleHover}
       />
     </>
   );
