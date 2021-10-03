360d6647432d8231bfb2cac485bb23023dc93994
Sid Nair <siddharthnair87@outlook.com>
2021-10-03 13:15:06 +0100

    added conditional rendering for loading page

--- frontend/amazon/src/components/PopularMovies/index.js
+++ frontend/amazon/src/components/PopularMovies/index.js
@@ -3,18 +3,25 @@ import DisplayData from "../DisplayData";
 
 const PopularMovies = () => {
   const [data, setData] = useState(null);
+  const [isPending, setIsPending] = useState(true);
 
   useEffect(() => {
-    fetch("http://localhost:3000/shows/popularmovies")
-      .then((res) => {
-        return res.json();
-      })
-      .then((data) => setData(data.payload));
+    setTimeout(() => {
+      fetch("http://localhost:3000/shows/popularmovies")
+        .then((res) => {
+          return res.json();
+        })
+        .then((data) => {
+          setData(data.payload);
+          setIsPending(false);
+        });
+    }, 1000);
   }, []);
 
   return (
-    <div className="popularMovies" style={{ display: "inline" }}>
-      <DisplayData data={data} />
+    <div className="popularMovies">
+      {isPending && <div>Loading ...</div>}
+      {data && <DisplayData data={data} />}
     </div>
   );
 };
